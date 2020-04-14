1970346f10e1d11b72f6ea9b010beb2f7a494883
StasPI <alons@bk.ru>
2020-04-15 02:02:14 +0300

    upload_new_file

---
+++ Автоматизация тестирования с помощью Selenium и Python/test_abs_project.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
+
+def test_abs3():
+    assert abs(-22) == 22, "Should be absolute value of a number"
+
+
+if __name__ == "__main__":
+    unittest.main()
